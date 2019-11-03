ee5a60e4a33ffedae188dda362bdd78a52405a56
AnusReaper <49763556+AnusReaper@users.noreply.github.com>
2019-11-03 12:34:51 +0000

    Initial commit

---
+++ js/js.js
@@ -0,0 +1,30 @@
+const button = document.querySelector("#button");
+const github = document.querySelector("#button2");
+const icon = document.querySelector("#button > i");
+const audio = document.querySelector("audio");
+
+$(function () {
+    $('.marquee').marquee({
+        duration: 5000,
+		pauseOnHover: true
+    });
+});
+
+button.addEventListener("click", () => {
+  if (audio.paused) {
+	audio.volume = 1;
+	audio.play();
+	icon.classList.remove('fa-volume-up');
+	icon.classList.add('fa-volume-mute');
+	button.classList.add("fade");
+	button2.classList.add("fade");
+  } else {
+	audio.pause();
+	icon.classList.remove('fa-volume-mute');
+	icon.classList.add('fa-volume-up');
+	button.classList.remove("fade");
+	button2.classList.remove("fade");
+  }
+});
+
+document.addEventListener('contextmenu', event => event.preventDefault());

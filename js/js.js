d2e54bba341b424316bf6ecff42591731e11e9d3
AnusReaper <49763556+AnusReaper@users.noreply.github.com>
2019-11-03 15:22:38 +0000

    Forced all JavaScript to run when the page is ready

--- js/js.js
+++ js/js.js
@@ -3,28 +3,28 @@ const github = document.querySelector("#button2");
 const icon = document.querySelector("#button > i");
 const audio = document.querySelector("audio");
 
-$(function () {
+$(document).ready(function () {
     $('.marquee').marquee({
         duration: 5000,
 		pauseOnHover: true
     });
-});
+	
+	button.addEventListener("click", () => {
+	  if (audio.paused) {
+		audio.volume = 1;
+		audio.play();
+		icon.classList.remove('fa-volume-up');
+		icon.classList.add('fa-volume-mute');
+		button.classList.add("fade");
+		button2.classList.add("fade");
+	  } else {
+		audio.pause();
+		icon.classList.remove('fa-volume-mute');
+		icon.classList.add('fa-volume-up');
+		button.classList.remove("fade");
+		button2.classList.remove("fade");
+	  }
+	});
 
-button.addEventListener("click", () => {
-  if (audio.paused) {
-	audio.volume = 1;
-	audio.play();
-	icon.classList.remove('fa-volume-up');
-	icon.classList.add('fa-volume-mute');
-	button.classList.add("fade");
-	button2.classList.add("fade");
-  } else {
-	audio.pause();
-	icon.classList.remove('fa-volume-mute');
-	icon.classList.add('fa-volume-up');
-	button.classList.remove("fade");
-	button2.classList.remove("fade");
-  }
+	document.addEventListener('contextmenu', event => event.preventDefault());
 });
-
-document.addEventListener('contextmenu', event => event.preventDefault());

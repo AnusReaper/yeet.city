985d8968400d9f2f40884f9faf7dc40551adb66d
AnusReaper <49763556+AnusReaper@users.noreply.github.com>
2019-11-06 15:03:08 +0000

    Removed the old jQuery syntax

--- js/js.js
+++ js/js.js
@@ -3,27 +3,25 @@ const github = document.querySelector("#button2");
 const icon = document.querySelector("#button > i");
 const audio = document.querySelector("audio");
 
-$(document).ready(function () {
-	button.addEventListener("click", () => {
-	  if (audio.paused) {
-		audio.volume = 1;
-		audio.play();
-		icon.classList.remove('fa-volume-up');
-		icon.classList.add('fa-volume-mute');
-		button.classList.add("fade");
-		button2.classList.add("fade");
-		button3.classList.add("fade");
-		document.title = 'NP: Autumn Voyage'
-	  } else {
-		audio.pause();
-		icon.classList.remove('fa-volume-mute');
-		icon.classList.add('fa-volume-up');
-		button.classList.remove("fade");
-		button2.classList.remove("fade");
-		button3.classList.remove("fade");
-		document.title = 'yeet.city'
-	  }
-	});
-
-	document.addEventListener('contextmenu', event => event.preventDefault());
+button.addEventListener("click", () => {
+  if (audio.paused) {
+	audio.volume = 1;
+	audio.play();
+	icon.classList.remove('fa-volume-up');
+	icon.classList.add('fa-volume-mute');
+	button.classList.add("fade");
+	button2.classList.add("fade");
+	button3.classList.add("fade");
+	document.title = 'NP: Autumn Voyage'
+  } else {
+	audio.pause();
+	icon.classList.remove('fa-volume-mute');
+	icon.classList.add('fa-volume-up');
+	button.classList.remove("fade");
+	button2.classList.remove("fade");
+	button3.classList.remove("fade");
+	document.title = 'yeet.city'
+  }
 });
+
+document.addEventListener('contextmenu', event => event.preventDefault());
